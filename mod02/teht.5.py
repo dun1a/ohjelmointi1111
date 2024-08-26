4eb114034ef092962420d073cd2b71e3512c8114
Donya <donyaa@metropolia.fi>
2024-08-26 10:06:14 +0300

    oikea vastaus

--- mod02/teht.5.py
+++ mod02/teht.5.py
@@ -1,11 +1,25 @@
 
-leiviska = int(input("Anna leivistkat:  "))
-naula = int(input("Anna naulat: "))
-luoti = float(input("Anna luotit: "))
+leiviskät = int(input("Anna leivistkat:  "))
+naulat = int(input("Anna naulat: "))
+luodit = float(input("Anna luotit: "))
 
-print("Massa nykymittojen mukaan: ")
-summa = leiviska + naula + luoti
-print(summa , "kilogrammaa")
+# ensn selvitetään kuinka paljon on nauloja
+naulatot = leiviskät * 20 + naulat
 
+# sitten selvitetään kuinka paljon meillä on luoteja yhteensä
+luoditot = naulatot * 32 + luodit
 
+# nyt kun tiedetään määrän niin lasketaan massa ensin grammoina
+grammatot = luoditot * 13.3
+print(grammatot)
 
+kg = grammatot / 100
+print('kg' , kg)
+
+kg = int(grammatot / 1000)
+print('kg' , kg)
+
+# on olemassa jakojäännösoperaatio (%), pelkän kokonaisluvun
+kg = int(grammatot // 1000)
+gr = grammatot % 1000
+print(f'massa nykymittojen mukaan: {kg} ja {gr:.2f} grammaa')
