29d6d343ededc36773c6775ca4ebdec7f25cd6b6
Donya <donyaa@metropolia.fi>
2024-10-25 18:22:16 +0300

    korjattu

--- mod09/mod9.3.py
+++ mod09/mod9.3.py
@@ -1,10 +1,7 @@
 # Laajenna ohjelmaa siten, että mukana on kulje-metodi, joka saa parametrinaan tuntimäärän. Metodi kasvattaa kuljettua matkaa sen verran kuin auto on tasaisella vauhdilla annetussa tuntimäärässä edennyt.
 # Esimerkki: auto-olion tämänhetkinen kuljettu matka on 2000 km. Nopeus on 60 km/h. Metodikutsu auto.kulje(1.5) kasvattaa kuljetun matkan lukemaan 2090 km.
 
-
-
-
-class Auto:
+class Auto
     def __init__(self, rekisteritunnus, huippunopeus, nopeus=0, kuljettu_matka=0):
         self.rekisteritunnus = rekisteritunnus
         self.huippunopeus = huippunopeus
@@ -34,5 +31,4 @@ auto.nopeus = 60
 auto.kuljettu_matka = 2000
 auto.kulje(1.5)
 auto.aunton_ominaisuudet()
-
 
