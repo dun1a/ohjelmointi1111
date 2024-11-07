858fcae7d01431e45f98bde56a1b0929d84f2dea
Donya <donyaa@metropolia.fi>
2024-11-07 23:09:12 +0200

    .

---
+++ mod13/mod13.teth.1.py
@@ -0,0 +1,44 @@
+from flask import Flask, Response, render_template
+import json
+import alkulukuteht
+
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def tehtava():
+    return 'alkulukutehtävä'
+
+# http://127.0.0.1:3000/alkuluku/31
+
+@app.route('/alkuluku/<alkuluku>')
+def alkuluku(alkuluku):
+    try:
+        luku = int(alkuluku)
+        # laske tässä alkuluku TAI kutsu funktio toisesta tiedostosta
+        alkuluku = alkulukuteht.laske(luku)
+
+        tilakoodi = 200
+
+        #{'Number': 31, 'isPrime':True}
+
+        vastaus = {
+            "Number": luku,
+            "isPrime": alkuluku
+        }
+
+    except ValueError:
+        tilakoodi = 400
+        vastaus = {
+            "status": tilakoodi,
+            "teksti": "Virheellinen luku alkuluku ei voida laksea "
+        }
+
+    jsonvast = json.dumps(vastaus)
+    return Response(response=jsonvast, status=tilakoodi, mimetype="application/json")
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
