144268b95226ec05c6414864dca0b91a42be9619
Donya <donyaa@metropolia.fi>
2024-11-08 00:09:25 +0200

    .

--- mod13/mod13.esim2..py
+++ mod13/mod13.esim2..py
@@ -39,7 +39,8 @@ def page_not_found(virhekoodi):
         "teksti" : "Virheellinen päätepiste"
     }
     jsonvast = json.dumps(vastaus)
-    return Response(response=jsonvast, status=404, mimetype="application/json")
+
+    #return Response(response=jsonvast, status=404, mimetype="application/json")
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
